9f42819c0a156cd7905d5d60917c84ba7b4ad63e
naojinerette <naojimagot@gmail.com>
2023-06-04 20:32:04 -0400

    listado de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './shared/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { MaterialModule } from './material.module';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './services/products.service';
+import { tap } from 'rxjs/operators';
+import { Product } from './interface/product.interface';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productsSvc: ProductsService) { }
 
   ngOnInit(): void {
+    this.productsSvc.getProducts()
+    .pipe(
+      tap((products: Product[]) => this.products = products)
+    )
+    .subscribe();
   }
 
 }
